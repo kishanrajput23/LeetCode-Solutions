5056a691e1ac39bd0b13bc033706951e754145f1
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-22 22:52:44 +0530

    Search Insert Position

---
+++ Python/35.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l,r=0,len(nums)
+        while l<r:
+            mid=l+(r-l)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                r=mid
+            else:
+                l=mid+1
+        return l
