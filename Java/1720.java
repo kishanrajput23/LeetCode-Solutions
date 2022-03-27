c6eb01ffb38bf5f8b47feb1af097b5825283ab75
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-27 18:07:51 +0530

    Decode XORed Array

---
+++ Java/1720.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int n = encoded.length;
+        int res[] = new int[n + 1];
+        res[0] = first;
+        for (int i=0; i<n; i++)
+            res[i+1] = res[i] ^ encoded[i];
+        return res;
+    }
+}
