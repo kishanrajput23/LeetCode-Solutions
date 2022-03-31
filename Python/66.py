7f433f2d7b64cb49b30ad1d06c9d88eb8c0eddd7
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-03-31 22:13:47 +0530

    Plus One

---
+++ Python/66.py
@@ -0,0 +1,9 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        result = []
+        str_digits = ''.join(str(i) for i in digits)
+        int_digits = int(str_digits) + 1
+        str_digits = str(int_digits)
+        for i in str_digits:
+            result.append(int(i))
+        return result
