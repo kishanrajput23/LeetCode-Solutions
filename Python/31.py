756e7e65141385a45c446855814500d072c273a8
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-04-03 22:08:06 +0530

    Next Permutation

---
+++ Python/31.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = j = len(nums)-1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1 
+        if i == 0:
+            nums.reverse()
+            return 
+        while nums[j] <= nums[i-1]:
+            j -= 1
+        nums[i-1], nums[j] = nums[j], nums[i-1]
+        nums[i:]= nums[len(nums)-1:i-1:-1]
+        
