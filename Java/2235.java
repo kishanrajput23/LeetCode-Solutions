986373e0a72309bb03cb9879be37af51b71c1aad
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-04-15 16:41:25 +0530

    Add Two Integers

---
+++ Java/2235.java
@@ -0,0 +1,9 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
+        
