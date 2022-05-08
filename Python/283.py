9817496bffd669719491b3b551c791876e3e82a1
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-05-08 22:46:05 +0530

    Move Zeroes

---
+++ Python/283.py
@@ -0,0 +1,6 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(key= lambda x: 1 if x == 0 else 0)
