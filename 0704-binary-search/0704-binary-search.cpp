5e036bb4178bacc68ede547162c96e5448b81f6f
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-07-13 21:43:45 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.38%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
