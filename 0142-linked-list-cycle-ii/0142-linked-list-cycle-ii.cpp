fdf1cbdde15d3b5764984d83787752516812d148
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-07-18 19:42:08 +0530

    Time: 5 ms (83.57%), Space: 11.4 MB (55.16%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -1,40 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ * };
+ */
+
 class Solution {
 public:
-
-    ListNode* floydDetectCycle(ListNode* head) {
+    ListNode *detectCycle(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) {
+            return nullptr;
+        }
 
         ListNode* slow = head;
         ListNode* fast = head;
 
+        // Step 1: Detect if a cycle exists
         while (fast != nullptr && fast->next != nullptr) {
-
             slow = slow->next;
             fast = fast->next->next;
 
-            if (slow == fast)
-                return slow;
+            if (slow == fast) {
+                // Step 2: Find the start of the cycle
+                ListNode *start = head;
+
+                while (start != slow) {
+                    start = start->next;
+                    slow = slow->next;
+                }
+
+                return start;
+            }
         }
 
         return nullptr;
-    }
-
-    ListNode* detectCycle(ListNode* head) {
-
-        if (head == nullptr)
-            return nullptr;
-
-        ListNode* meetingPoint = floydDetectCycle(head);
-
-        if (meetingPoint == nullptr)
-            return nullptr;
-
-        ListNode* slow = head;
-
-        while (slow != meetingPoint) {
-            slow = slow->next;
-            meetingPoint = meetingPoint->next;
-        }
-
-        return slow;
     }
 };
