271feb5faafb89e01c1adedeeb49a988a717fd69
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-07-18 19:34:35 +0530

    Time: 3 ms (98.12%), Space: 10.2 MB (99.98%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -1,21 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* detectCycle(ListNode* head) {
+    ListNode* floydDetectCycle(ListNode* &head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        unordered_set<ListNode*> visited;
+        while (slow != NULL && fast != NULL) {
+            fast = fast->next;
+            if (fast != NULL) {
+                fast = fast->next;
+            }
+            slow = slow->next;
 
-        ListNode* curr = head;
-
-        while (curr != nullptr) {
-
-            if (visited.count(curr))
-                return curr;
-
-            visited.insert(curr);
-
-            curr = curr->next;
+            if (slow == fast) {
+                return slow;
+            }
+        }
+        return NULL;
+    }
+    ListNode *detectCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL) {
+            return NULL;
         }
 
-        return nullptr;
+        ListNode* intersection = floydDetectCycle(head);
+        if (intersection == NULL) {
+            return NULL;
+        }
+
+        ListNode* slow = head;
+
+        while (intersection != slow) {
+            intersection = intersection->next;
+            slow = slow->next;
+        }
+        return slow;
     }
 };
