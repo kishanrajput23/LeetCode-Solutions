7799ad7eb7289a4e9d70b2d54cc722fe4f1bd352
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-07-28 22:24:18 +0530

    Time: 3 ms (100%), Space: 75.8 MB (33.63%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,48 +2,34 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
 
-        // Frequency array to count occurrences of each character
+        // Store frequency of each character
         vector<int> freq(26, 0);
 
-        // Count frequency of every character
-        for (int i = 0; i < s.size(); i++) {
-            freq[s[i] - 'a']++;
+        // Count frequencies
+        for (char ch : s) {
+            freq[ch - 'a']++;
         }
 
         string left = "";
-
-        // Build the left half of the palindrome
-        for (int i = 0; i < 26; i++) {
-
-            // Half of the occurrences go to the left side
-            int n = freq[i] / 2;
-
-            // Keep only the remaining (0 or 1) occurrence
-            // for the middle character
-            freq[i] = freq[i] % 2;
-
-            // Add the current character 'n' times
-            while (n--) {
-                left += (i + 'a');
-            }
-        }
-
         string middle = "";
 
-        // Build the middle part (only characters with odd frequency remain)
+        // Build the left half and identify the middle character
         for (int i = 0; i < 26; i++) {
-            int n = freq[i];
 
-            while (n--) {
-                middle += (i + 'a');
+            // Add half of the occurrences to the left half
+            left.append(freq[i] / 2, char(i + 'a'));
+
+            // If frequency is odd, one character stays in the middle
+            if (freq[i] % 2 == 1) {
+                middle += char(i + 'a');
             }
         }
 
-        // Right half is simply the reverse of the left half
+        // Right half is the mirror image of the left half
         string right = left;
         reverse(right.begin(), right.end());
 
-        // Combine all three parts
+        // Form the smallest palindromic rearrangement
         return left + middle + right;
     }
 };
