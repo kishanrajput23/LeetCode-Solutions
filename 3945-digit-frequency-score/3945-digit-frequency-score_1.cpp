6662af3275d0faf204e98d1ac517901af2bf7889
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-02 14:21:16 +0530

    Time: 0 ms (100.00%) | Space: 8.6 MB (58.41%) - CodeHub Sync

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score_1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        int ans = 0;
+
+        while (n > 0) {
+            ans += n % 10;
+            n /= 10;
+        }
+
+        return ans;
+    }
+};
