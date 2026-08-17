168ade91b92ca29ee66c0c141fae293f5c986be4
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-17 22:34:03 +0530

    Time: 0 ms (100.00%) | Space: 20.9 MB (5.20%) - CodeHub Sync

---
+++ Solutions/1920-build-array-from-permutation/1920-build-array-from-permutation_1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ans;
+        
+        for (int i=0; i<n; i++) {
+            ans.push_back(nums[nums[i]]);
+        }
+
+        return ans;
+    }
+};
