794f1feab684605da8773f4357349f4127fb0ef4
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-21 23:19:18 +0530

    Time: 0 ms (100.00%) | Space: 17.7 MB (46.08%) - CodeHub Sync

---
+++ Solutions/2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations_1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+
+        for (int i=0; i<operations.size(); i++) {
+            if (operations[i] == "X++") {
+                x++;
+            }
+            else if (operations[i] == "--X") {
+                --x;
+            }
+            else if (operations[i] == "X--") {
+                x--;
+            }
+            else {
+                ++x;
+            }
+        }
+        return x;
+    }
+};
