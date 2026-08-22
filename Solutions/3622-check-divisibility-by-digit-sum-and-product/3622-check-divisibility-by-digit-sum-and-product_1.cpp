9d4cd08c25606f27cdada84524ddbcb658cae098
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-22 10:46:39 +0530

    Time: 0 ms (N/A%) | Space: 7.9 MB (N/A%) - CodeHub Sync

---
+++ Solutions/3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product_1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int num = n;
+        int sum = 0;
+        int prod = 1;
+
+        while (num > 0) {
+            sum += n % 10;
+
+            prod *= n % 10;
+
+            num /= 10;
+        }
+
+        int total = sum + prod;
+
+        if (n % total == 0) {
+            return true;
+        }
+
+        return false;
+    }
+};
