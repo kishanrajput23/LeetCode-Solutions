fc674a243cda60159b9b427c933246cfa2825906
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-09-14 12:53:01 +0530

    Time: 0 ms (100.00%) | Space: 11.4 MB (54.97%) - CodeHub Sync

---
+++ Solutions/1672-richest-customer-wealth/1672-richest-customer-wealth_1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxi = 0;
+        
+        for (int i=0; i<accounts.size(); i++) {
+            int sum  = 0;
+            for (int j=0; j<accounts[i].size(); j++) {
+                sum += accounts[i][j];
+            }
+            if (maxi<sum) {
+                maxi = sum;
+            }
+        }
+        return maxi;
+    }
+};
